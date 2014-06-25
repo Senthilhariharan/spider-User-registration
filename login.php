6a1966cd9f4fbb75c0ed30dd8e0d36309e4a7b37
Senthilhariharan <ninjaharry96@gmail.com>
2014-06-25 16:27:26 +0530

    Update login.php

--- login.php
+++ login.php
@@ -17,8 +17,8 @@ if(isset($_POST["submit"])){
 
 if(!empty($_POST['user']) && !empty($_POST['pass'])) {
 	$user=$_POST['user'];
-	$pass=$_POST['pass'];
-
+	
+$pass=sha1($_POST['pass']);
 	$con=mysql_connect('localhost','root','') or die(mysql_error());
 	mysql_select_db('user_registration') or die("cannot select DB");
 
